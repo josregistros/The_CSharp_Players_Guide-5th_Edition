eb14f1c6d9720849b2ad21f43a8516d830fdf3b7
josregistros <josregistros@gmail.com>
2025-12-17 20:44:37 +0100

    The Defense Of Consolas

---
+++ The_Defense_Of_Consolas/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Target Row? ");
+string targetRow = Console.ReadKey().KeyChar.ToString();
+
+Console.Write("\nTarget Column? ");
+string targetColumn = Console.ReadKey().KeyChar.ToString();
+
+Console.WriteLine("\nDeploy to:");
+
+int targetColumnMinusOne = int.Parse(targetColumn) - 1;
+int targetColumnPlusOne = int.Parse(targetColumn) + 1;
+
+Console.WriteLine($"({targetRow}, {targetColumnMinusOne})");
+Console.WriteLine($"({targetRow}, {targetColumnPlusOne})");
+
+int targetRowMinusOne = int.Parse(targetRow) - 1;
+int targetRowPlusOne = int.Parse(targetRow) + 1;
+
+Console.WriteLine($"({targetRowMinusOne}, {targetColumn})");
+Console.WriteLine($"({targetRowPlusOne}, {targetColumn})");
