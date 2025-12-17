abf6d6d1f2d053700c823596cc21da1e9c7d53d8
josregistros <josregistros@gmail.com>
2025-12-17 18:09:26 +0100

    variables and Console lines

---
+++ Consolas_and_Telim/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a;
+int b;
+a = 5;
+b = 2;
+b = a;
+a = -3;
+
+Console.BackgroundColor = ConsoleColor.Yellow; 
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string name = Console.ReadLine();
+Console.WriteLine(name + " got bread.");
+
+Console.WriteLine("What kind of thing are we talking about?");
+string x = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string y = Console.ReadLine();
+string c = "of Doom";
+string d = "3000";
+Console.WriteLine("The " + y + " " + x + " of " + c + " " + d + "!");
