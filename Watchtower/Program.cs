f345d2a56b2e58b025078dd639d3f77aa3a397df
josregistros <josregistros@gmail.com>
2025-12-18 19:27:50 +0100

    Watchtower

---
+++ Watchtower/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Write value for x: ");
+string xValue = Console.ReadLine();
+
+Console.Write("Write value for y: ");
+string yValue = Console.ReadLine();
+
+int x = int.Parse(xValue);
+int y = int.Parse(yValue);
+
+if ( x == 0 && y == 0)
+    Console.WriteLine("The enemy is here!");
+else if (x == 0 &&  y > 0)
+    Console.WriteLine("The enemy is to the north!");
+else if (x == 0 && y < 0)
+    Console.WriteLine("The enmy is to the south!");
+else if (x < 0 && y > 0)
+    Console.WriteLine("The enemy is to the northwest");
+else if (x < 0 && y == 0)
+    Console.WriteLine("The enemy is to the west");
+else if (x < 0 && y < 0)
+    Console.WriteLine("The enemy is to the southwest");
+else if (x > 0 && y > 0)
+    Console.WriteLine("The enemy is to the northeast");
+else if (x > 0 && y == 0)
+    Console.WriteLine("The enemy is to the east");
+else if (x > 0 && y < 0)
+    Console.WriteLine("The enemy is to the southeast");
